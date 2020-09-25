3b1fc7c306b015d7c1905c33581d0e55bfc70429
zapzrat <zapzrat@gmail.com>
2020-09-25 12:20:36 +0700

    Add files via upload

---
+++ Code/HC-SR501_PIR_Motion/HC-SR501_PIR_Motion.ino
@@ -0,0 +1,33 @@
+const int pir = 12;
+const int ledPin =  13;
+
+int statusActive = 0 ;
+int statusPush = 0;
+int pirState = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Micro Project Thailand ~^.^~ ");
+  pinMode(ledPin, OUTPUT);
+  pinMode(pir, INPUT);
+}
+
+void loop() {
+  pirState = digitalRead(pir);
+
+  Serial.print("pirState : "); Serial.println(pirState);
+
+  if (pirState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+//      Serial.println("HHHHHH");
+  }
+
+  else if (pirState == LOW) {
+    digitalWrite(ledPin, LOW);
+
+  }
+
+
+
+
+}
